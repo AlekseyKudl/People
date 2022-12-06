8198728372ed33e44af37d459e675f099320e574
AlekseyKudl <a.kudlaev@gmail.com>
2022-12-06 17:12:42 +0300

    Задание 3

--- src/Human.java
+++ src/Human.java
@@ -9,11 +9,60 @@ public class Human {
         this.name = name;
         this.town = town;
     }
+
+    public void setBirthYear(int birthYear) {
+        if (birthYear < 0){
+            this.birthYear = 0;
+        }else {
+            this.birthYear = birthYear;
+        }
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+    }
+
+    public void setTown(String town){
+            if (town == null) {
+                this.name = "Информация не указана";
+            } else {
+                this.town = town;
+            }
+        }
+
+    public void setJobTitle(String jobTitle) {
+        if (jobTitle == null) {
+            this.name = "Информация не указана";
+        }else {
+            this.jobTitle = jobTitle;
+        }
+    }
+
     Human (int birthYear, String name, String town, String jobTitle){
-        this.birthYear = birthYear;
-        this.name = name;
-        this.town = town;
-        this.jobTitle = jobTitle;
+        if (birthYear < 0){
+            this.birthYear = 0;
+        }else {
+            this.birthYear = birthYear;
+        }
+        if (name == null){
+            this.name = "Информация не указана";
+        }else {
+            this.name = name;
+        }
+        if (town == null) {
+            this.name = "Информация не указана";
+        }else {
+            this.town = town;
+        }
+        if (jobTitle == null) {
+            this.name = "Информация не указана";
+        }else {
+            this.jobTitle = jobTitle;
+        }
     }
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.sql.SQLOutput;
 
 public class Main {
+    public static final Auto[] AUTOS = new Auto[11];
     public static void main(String[] args) {
     Human maxim = new Human(1988, "Максим", "Минск");
     Human anna = new Human(1993, "Аня", "Москва");
@@ -20,7 +22,46 @@ public class Main {
         anna.hello2();
         kate.hello2();
         artem.hello2();
+// задание 3
+        System.out.println("_________________________________________");
 
+        AUTOS[0]= new Auto("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
+        AUTOS[1] = new Auto("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+        //BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
+        AUTOS[2]= new Auto("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+        //Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
+        AUTOS[3] = new Auto("KIA", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        //Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год.
+        AUTOS[4] = new Auto("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        printFullinfo();
+
+        //задание_3
+        System.out.println("_______________________________________");
+        maxim.setName(null);
+        anna.setJobTitle(null);
+        kate.setBirthYear(-1999);
+        artem.setTown(null);
+        maxim.hello2();
+        anna.hello2();
+        kate.hello2();
+        artem.hello2();
+        System.out.println("_____________________________________________________");
+
+        AUTOS[5] = new Auto(null, "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        AUTOS[6] = new Auto("Hyundai", null, - 1.0, "оранжевый", 2016, "Южная Корея");
+        AUTOS[7] = new Auto("Hyundai", "Avante", 1.6, null, 2016, "Южная Корея");
+        AUTOS[8] = new Auto("Hyundai", "Avante", 0, "оранжевый", -100, "Южная Корея");
+        AUTOS[9] = new Auto("Hyundai", "Avante", 1.6, "оранжевый", 0, null);
+        AUTOS[10] = new Auto("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+
+        printFullinfo();
+    }
+    private static void printFullinfo() {
+        for (Auto auto : AUTOS) {
+            if (auto != null) {
+                System.out.println(auto);
+            }
+        }
     }
 
 }
