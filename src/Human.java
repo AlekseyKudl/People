1ed4d59688949f868adbf23c172a8868cb2af31e
AlekseyKudl <a.kudlaev@gmail.com>
2022-12-06 14:01:05 +0300

    Задание 1

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,19 @@
 public class Human {
+    int birthYear;
+    String name;
+    String town;
+    Human (int birthYear, String name, String town){
+        this.birthYear = birthYear;
+        this.name = name;
+        this.town = town;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    void hello(){
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town +". " +
+                "Я родился в " + birthYear + " году. Будем знакомы!");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Human maxim = new Human(1988, "Максим", "Минск");
+    maxim.hello();
+    Human anna = new Human(1993, "Аня", "Москва");
+    anna.hello();
+    Human kate = new Human(1992, "Катя", "Калининград");
+    kate.hello();
+    Human artem = new Human(1995, "Артем", "Москва");
+    artem.hello();
     }
+
 }
